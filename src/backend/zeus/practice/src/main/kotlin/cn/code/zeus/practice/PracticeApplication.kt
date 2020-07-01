848f73394bb2081960cbcdf88632308c9b8ebb39
dell <dell@canway.net>
2020-07-01 17:08:21 +0800

    feature:集成apollo

--- src/backend/zeus/practice/src/main/kotlin/cn/code/zeus/practice/PracticeApplication.kt
+++ src/backend/zeus/practice/src/main/kotlin/cn/code/zeus/practice/PracticeApplication.kt
@@ -1,9 +1,11 @@
 package cn.code.zeus.practice
 
 import cn.code.zeus.common.web.anno.MicroService
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig
 import org.springframework.boot.SpringApplication
 
 @MicroService
+@EnableApolloConfig
 class PracticeApplication
 
 fun main(args: Array<String>) {
--- src/backend/zeus/practice/src/main/kotlin/cn/code/zeus/practice/controller/IndexController.kt
+++ src/backend/zeus/practice/src/main/kotlin/cn/code/zeus/practice/controller/IndexController.kt
@@ -5,6 +5,7 @@ import cn.code.zeus.user.api.service.ServiceUserApi
 import cn.code.zeus.user.pojo.UserInfo
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,22 +19,30 @@ class IndexController {
     @Resource
     lateinit var serviceUserApi: ServiceUserApi
 
+    @Value("\${app.conf}")
+    lateinit var conf:String
 
     @RequestMapping("/user/default")
     fun getDefaultUserInfo(): String {
         return serviceUserApi.getDefaultUserInfo()
     }
 
-    @RequestMapping("/{type}")
-    fun index(
-        @PathVariable("type")
-        type: String
-    ): String {
-        logger.info(type)
-        if (type == "400")
-            throw RequestException("参数不匹配")
-        return "hello"
+    @RequestMapping("/conf")
+    fun conf():String{
+        return conf
     }
+
+
+//    @RequestMapping("/{type}")
+//    fun index(
+//        @PathVariable("type")
+//        type: String
+//    ): String {
+//        logger.info(type)
+//        if (type == "400")
+//            throw RequestException("参数不匹配")
+//        return "hello"
+//    }
 
 
     companion object {
