20d499182bb057423bac37a5a495aaf1520b6324
didoodidoo <drdiaorui@163.com>
2020-07-26 10:50:39 +0800

    feature: LeetCode每日一题: 329

---
+++ src/leet/src/test/java/cn/code/leet/Solution_329Test.java
@@ -0,0 +1,19 @@
+package cn.code.leet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_329Test {
+
+    private Solution_329 solution = new Solution_329();
+
+    @Test
+    void longestIncreasingPath() {
+        int[][] arr = new int[3][3];
+        arr[0] = new int[]{9, 9, 4};
+        arr[1] = new int[]{6, 6, 8};
+        arr[2] = new int[]{2, 1, 1};
+        assertEquals(4,solution.longestIncreasingPath(arr));
+    }
+}
