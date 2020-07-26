7093c02011af85399295274b8974bb86360d831a
didoodidoo <drdiaorui@163.com>
2020-07-26 12:46:45 +0800

    feature: LeetCode每日一题: 2

---
+++ src/leet/src/main/java/cn/code/leet/Solution_2.java
@@ -0,0 +1,58 @@
+package cn.code.leet;
+
+public class Solution_2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(l1.val + l2.val);
+        ListNode tail = head;
+        while (l1.next != null || l2.next != null) {
+            int sum = 0;
+            if (l1.next != null) {
+                l1 = l1.next;
+                sum += l1.val;
+            }
+            if (l2.next != null) {
+                l2 = l2.next;
+                sum += l2.val;
+            }
+            tail.next = new ListNode(sum);
+            tail = tail.next;
+        }
+        parseList(head);
+        return head;
+    }
+
+    private void parseList(ListNode list) {
+//        记录低位是否要像高位进一
+        ListNode p = list;
+        boolean advance = false;
+
+        while (p.next != null) {
+            if (advance == true)
+                p.val = p.val + 1;
+            if (p.val >= 10) {
+                p.val = p.val % 10;
+                advance = true;
+            } else {
+                advance = false;
+            }
+            p = p.next;
+        }
+//  处理尾节点
+        if (advance)
+            p.val = p.val + 1;
+        if (p.val >= 10) {
+            p.val = p.val % 10;
+            p.next = new ListNode(1);
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/leet/src/test/java/cn/code/leet/Solution_2Test.java
@@ -0,0 +1,27 @@
+package cn.code.leet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_2Test {
+
+    private Solution_2 solution = new Solution_2();
+
+    @Test
+    void addTwoNumbers() {
+        ListNode l1 = new ListNode(2);
+        ListNode l11 = new ListNode(4);
+        ListNode l12 = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        ListNode l21 = new ListNode(6);
+        ListNode l22 = new ListNode(4);
+        l1.next = l11;
+        l11.next = l12;
+        l2.next = l21;
+        l21.next = l22;
+
+        solution.addTwoNumbers(l1,l2);
+
+    }
+}
