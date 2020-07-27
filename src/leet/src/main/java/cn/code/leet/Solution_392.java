047bef54c7b7624df7a1a316d9e30b40bc1521c1
dell <dell@canway.net>
2020-07-27 09:59:40 +0800

    feature: LeetCode每日一题 392

---
+++ src/leet/src/main/java/cn/code/leet/Solution_392.java
@@ -0,0 +1,30 @@
+package cn.code.leet;
+
+public class Solution_392 {
+
+    public boolean isSubsequence(String s, String t) {
+//        两个指针 一个指s 一个指t 往后找就行
+//        if(s.length()==1)
+//            return t.contains(s);
+        int sP = 0;
+        int tP = 0;
+        while (sP < s.length() && tP < t.length()) {
+            boolean eq = false;
+            while (tP < t.length()) {
+                if (s.charAt(sP) == t.charAt(tP)) {
+//                    找到了直接跳出 指针不用在这里后移了
+                    eq = true;
+                    break;
+                }
+                tP++;
+            }
+//            说明直到越界都没有找到
+            if (!eq)
+                return false;
+//            找到了的话指针后移
+            sP++;
+            tP++;
+        }
+        return sP == s.length();
+    }
+}
---
+++ src/leet/src/test/java/cn/code/leet/Solution_392Test.java
@@ -0,0 +1,15 @@
+package cn.code.leet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_392Test {
+
+    private Solution_392 solution = new Solution_392();
+
+    @Test
+    void isSubsequence() {
+        System.out.println(solution.isSubsequence("a","c"));
+    }
+}
