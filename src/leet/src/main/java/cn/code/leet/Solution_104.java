ab8012dba62b00cced53bcf00aa6be9ce14e0fc4
dell <dell@canway.net>
2020-07-28 09:34:42 +0800

    feature: LeetCode每日一题 104

---
+++ src/leet/src/main/java/cn/code/leet/Solution_104.java
@@ -0,0 +1,26 @@
+package cn.code.leet;
+
+public class Solution_104 {
+//    一个简单的递归
+//    高度等于max(左子树的高度,右子树的高度) +1   //他自己
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+//            加上自己的高度
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
