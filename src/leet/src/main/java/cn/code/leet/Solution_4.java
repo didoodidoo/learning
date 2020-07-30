46f89bf75692ddbc4acaf4daa3d360daf12710b9
dell <dell@canway.net>
2020-07-30 15:29:20 +0800

    feature: LeetCode每日一题 5

---
+++ src/leet/src/main/java/cn/code/leet/Solution_4.java
@@ -0,0 +1,15 @@
+package cn.code.leet;
+
+public class Solution_4 {
+
+//    如果不要求log(m+n) 把两个数组合并排序取中位数就行了
+//    二分的不会
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        return 0;
+    }
+
+
+
+
+}
---
+++ src/leet/src/main/java/cn/code/leet/Solution_5.java
@@ -0,0 +1,48 @@
+package cn.code.leet;
+
+public class Solution_5 {
+//  用动态规划解
+//    [i,j]表示从i 到j 是否是一个回文子串
+//    我们知道  如果 [i-1,j+1]是一个回文子串的话  那么[i,j]也是一个回文子串
+//    由此后面的状态可以由前面的得到  即判断[i,j]是否是回文子串 再看一下前后的是否相同
+    public String longestPalindrome(String s) {
+        // 特判
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+
+//        先初始化对角线的 单个字符为true
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+}
