4e137eaef6ebd4b03e656de86dc4ae337a433d0a
dell <dell@canway.net>
2020-07-31 09:16:37 +0800

    feature: 华为面试题

---
+++ src/leet/src/main/java/cn/code/leet/Solution_HW.java
@@ -0,0 +1,26 @@
+package cn.code.leet;
+
+import java.util.*;
+
+public class Solution_HW {
+
+    public String solution(String s) {
+        StringBuffer result = new StringBuffer();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c))
+                map.put(c, 1);
+            else
+                map.put(c, map.get(c) + 1);
+        }
+        List<Map.Entry<Character, Integer>> nums = new ArrayList<>(map.entrySet());
+        nums.sort((o1, o2) -> o2.getValue() - o1.getValue());
+        for (Map.Entry<Character, Integer> n : nums) {
+            for (int i = 0; i < n.getValue(); i++) {
+                result.append(n.getKey());
+            }
+        }
+        return result.toString();
+    }
+}
+
---
+++ src/leet/src/test/java/cn/code/leet/Solution_HWTest.java
@@ -0,0 +1,27 @@
+package cn.code.leet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_HWTest {
+
+    private Solution_HW solution = new Solution_HW();
+
+    @Test
+    void solution() {
+        assertEquals("eert",solution.solution("tree"));
+    }
+
+    @Test
+    void solution_allSame() {
+        assertEquals("aaaccc",solution.solution("cccaaa"));
+    }
+
+
+    @Test
+    void solution_moreChar() {
+        assertEquals("bbAa",solution.solution("Aabb"));
+    }
+
+}
