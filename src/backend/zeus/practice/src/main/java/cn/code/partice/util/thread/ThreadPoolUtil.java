d883c43165889e240eb3866b9b7393a951537952
dell <dell@canway.net>
2020-08-28 14:32:41 +0800

    feature: 线程池练习

--- src/backend/zeus/practice/src/main/java/cn/code/partice/util/thread/ThreadPoolUtil.java
+++ src/backend/zeus/practice/src/main/java/cn/code/partice/util/thread/ThreadPoolUtil.java
@@ -2,6 +2,7 @@ package cn.code.partice.util.thread;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,6 +12,10 @@ public class ThreadPoolUtil {
     private static ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
 
     public static void execute(Runnable task) throws InterruptedException {
+
+        ConcurrentHashMap<String,String > m = new ConcurrentHashMap<String,String>();
+
+        m.put("a","a");
         int a = 2;
         int b = a >>> 2;
         fixedThreadPool.submit(task);
