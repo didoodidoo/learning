62872cb00e14ec7f285480e30e0de54df31b9910
dell <dell@canway.net>
2021-03-02 17:24:48 +0800

    feature: zk锁

--- src/backend/zeus/component/zookeeper/src/main/kotlin/cn/code/zeus/zk/controller/IndexController.kt
+++ src/backend/zeus/component/zookeeper/src/main/kotlin/cn/code/zeus/zk/controller/IndexController.kt
@@ -1,13 +1,13 @@
 package cn.code.zeus.zk.controller
 
 import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.framework.recipes.locks.InterProcessMutex
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/index")
@@ -15,7 +15,7 @@ class IndexController {
 
 
     @Autowired
-    lateinit var curatorFramework: CuratorFramework
+    lateinit var client: CuratorFramework
 
     @GetMapping("/val/{val}")
     fun setValue(
@@ -23,11 +23,25 @@ class IndexController {
             value: String
     ): String? {
         logger.info("try create node")
-        curatorFramework
+        client
                 .create()
                 .creatingParentsIfNeeded()
                 .forPath("/usr/default", value.toByteArray())
-        val v = curatorFramework.data.forPath("/usr/default")
+        val v = client.data.forPath("/usr/default")
+        return String(v)
+    }
+
+    @GetMapping("/lock/{val}")
+    fun lock(
+        @PathVariable("val")
+        value: String
+    ): String? {
+        logger.info("try lock for $value")
+        val lockPath = "/lock-default"
+        val lock = InterProcessMutex(client,lockPath)
+        lock.acquire()
+        lock.release()
+        val v = client.data.forPath("/usr/default")
         return String(v)
     }
 
